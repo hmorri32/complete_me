1706d69afb5ef39f396e41bf28fbacc542d587f7
Hugh Morrison <hmorri32@gmail.com>
2017-08-26 12:06:03 -0600

    implement ultra janky insert

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -1,5 +1,25 @@
 class CompleteMe
+  attr_accessor :count
+  
   def initialize
-    
+    @head = Node.new
+    @count = 0
+    @suggestions = []
+  end
+
+  def insert(word, node = @head)
+    return if word.empty? || word.nil?
+    letters = word.chars
+    letters.each.with_index do |letter, index|
+      letter = word[index]
+      if !node.children[letter]
+        node.children[letter] = Node.new
+      end
+      node = node.children[letter]
+      if index == word.length - 1
+        node.is_word = true
+        @count +=1
+      end
+    end
   end
 end
