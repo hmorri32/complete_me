baa8366779e747262bfab0a8d30757e92573a852
Hugh Morrison <hmorri32@gmail.com>
2017-08-28 21:41:21 -0600

    add word toggle methpd

--- lib/node.rb
+++ lib/node.rb
@@ -1,8 +1,13 @@
 class Node
-  attr_accessor :is_word, :children
+  attr_accessor :is_word, :children, :value
 
-  def initialize
+  def initialize(value)
+    @value    = value
     @is_word  = false
     @children = {}
+  end
+
+  def word_toggle 
+    @is_word = !@is_word
   end
 end
