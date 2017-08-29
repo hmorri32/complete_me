77519b4ef84630e384c481ffd9f1a8a04118be8c
Hugh Morrison <hmorri32@gmail.com>
2017-08-28 21:41:58 -0600

    delete, needs refactor

--- lib/trie.rb
+++ lib/trie.rb
@@ -12,9 +12,10 @@ class Trie
                 :suggestions
 
   def initialize
-    @head        = Node.new
+    @head        = Node.new(nil)
     @count       = 0
     @suggestions = []
+    @node_tank   = []
   end
 
   def insert(word, node = @head)
@@ -38,5 +39,45 @@ class Trie
     letters.each { |letter| node = node.children[letter] }
     find_word(node, prefix)
     @suggestions
+  end
+
+  def delete(word)
+    letters = word.downcase.chars
+    nodes   = letters.map {|letter| Node.new(letter)}
+    node    = find_node(nodes)
+    # return if node.nil?
+    node.word_toggle
+
+    node_pop(@node_tank)
+    
+    @count -= 1
+    @node_tank = []
+
+  end
+  
+  def node_pop(node_list)
+    node = node_list.pop
+    return if node.nil? || node.is_word
+
+    # delete_node(node, node_list) 
+
+    if node.children.empty?
+      parent = node_list.last
+      return if parent.nil?
+      parent.children.delete(node.value)
+    end
+    node_pop(node_list)
+  end
+
+  def find_parent(node, node_list)
+    node = node_list.last
+  end
+
+  def find_node(arr, parent = @head)
+    # return nil if parent.nil?
+    return @node_tank.last if arr.empty?
+    node = arr.shift 
+    @node_tank << parent.children[node.value]
+    find_node(arr, parent.children[node.value])
   end
 end
