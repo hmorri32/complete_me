570db8ce760e8e8386ff1d023f5e1f3db1ee6343
Hugh Morrison <hmorri32@gmail.com>
2017-08-29 08:03:44 -0600

    default value to nil

--- lib/node.rb
+++ lib/node.rb
@@ -1,13 +1,9 @@
 class Node
   attr_accessor :is_word, :children, :value
 
-  def initialize(value)
+  def initialize(value = nil)
     @value    = value
     @is_word  = false
     @children = {}
-  end
-
-  def word_toggle 
-    @is_word = !@is_word
   end
 end
